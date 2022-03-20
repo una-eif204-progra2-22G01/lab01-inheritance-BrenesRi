d0e6f580a25f62d6a6993899eb8cfe4ceabd0608
BrenesRi <ribrenes10@gmail.com>
2022-03-19 22:08:49 -0600

    Update Lab01Inheritance.cpp
    Segunda prueba de Source Code control

--- Lab01Inheritance.cpp
+++ Lab01Inheritance.cpp
@@ -1,5 +1,6 @@
 #include <iostream>  // allows program to output data to the screen
 //Esta es una prueba de Source Code Control
+//Esta es una segunda prueba de Source Code Control
 struct Base {
     virtual void do_something() {}
 };
